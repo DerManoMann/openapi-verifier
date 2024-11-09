2193970cadf1575f7bf4f1d34df77c010734cfd3
Martin Rademacher <mano@radebatz.net>
2024-11-09 14:04:40 +1300

    Update CS rules (#59)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
     ->path('src')->name('*.php')
@@ -9,16 +9,18 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
+        '@DoctrineAnnotation' => true,
         'array_syntax' => ['syntax' => 'short'],
         'no_unused_imports' => true,
         'blank_line_before_statement' => ['statements' => ['return']],
+        'visibility_required' => true,
         'cast_spaces' => ['space' => 'single'],
         'concat_space' => ['spacing' => 'one'],
-        'function_typehint_space' => true,
+        'type_declaration_spaces' => true,
         'lowercase_cast' => true,
         'magic_constant_casing' => true,
         'class_attributes_separation' => ['elements' => ['method' => 'one']],
-        'single_blank_line_before_namespace' => true,
+        'blank_lines_before_namespace' => true,
         'no_singleline_whitespace_before_semicolons' => true,
         'no_spaces_around_offset' => true,
         'no_whitespace_before_comma_in_array' => true,
@@ -28,18 +30,23 @@ return (new PhpCsFixer\Config())
         'no_blank_lines_after_phpdoc' => true,
         'no_extra_blank_lines' => true,
         'return_type_declaration' => ['space_before' => 'none'],
-        'no_trailing_comma_in_list_call' => true,
-        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_comma_in_singleline' => true,
         'no_unneeded_control_parentheses' => true,
-        'no_unneeded_curly_braces' => true,
+        'no_unneeded_braces' => true,
         'short_scalar_cast' => true,
         'space_after_semicolon' => true,
         'ternary_operator_spaces' => true,
         'trailing_comma_in_multiline' => true,
         'trim_array_spaces' => true,
+        'single_space_around_construct' => true,
+        'single_line_comment_spacing' => true,
+        'fully_qualified_strict_types' => true,
+        'global_namespace_import' => ['import_classes' => false, 'import_constants' => null, 'import_functions' => null],
+
+        'nullable_type_declaration_for_default_null_value' => true,
 
         'no_empty_phpdoc' => true,
-        // 7.3 only 'no_superfluous_phpdoc_tags' => true,
+        'no_superfluous_phpdoc_tags' => ['allow_mixed' => true],
         'phpdoc_align' => true,
         'general_phpdoc_tag_rename' => true,
         'phpdoc_inline_tag_normalizer' => true,
@@ -52,11 +59,11 @@ return (new PhpCsFixer\Config())
         'phpdoc_to_comment' => true,
         'phpdoc_summary' => true,
         'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_separation' => true,
+        'phpdoc_separation' => ['skip_unlisted_annotations' => true],
         'phpdoc_scalar' => true,
         'phpdoc_no_useless_inheritdoc' => true,
         'phpdoc_no_empty_return' => true,
         'phpdoc_no_alias_tag' => true,
     ])
     ->setFinder($finder)
-;
+    ;
