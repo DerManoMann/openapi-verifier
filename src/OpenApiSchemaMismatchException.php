08c7050d83e6acc6edd288db1b59a0592e0acf80
Daniel Jakob <github@usox.org>
2025-12-23 21:56:00 +0100

    Optionally allow JsonSchema 6.x (#66)

--- src/OpenApiSchemaMismatchException.php
+++ src/OpenApiSchemaMismatchException.php
@@ -2,15 +2,31 @@
 
 namespace Radebatz\OpenApi\Verifier;
 
+/**
+ * @phpstan-type JsonSchemaValidationError array{
+ *  property: string,
+ *  pointer: string,
+ *  message: string,
+ *  constraint: string|array{name: string, params: array<mixed>},
+ *  context: int
+ * }
+ */
 class OpenApiSchemaMismatchException extends \Exception
 {
-    protected $errors = [];
+    /** @var list<JsonSchemaValidationError> */
+    protected array $errors = [];
 
+    /**
+     * @return list<JsonSchemaValidationError>
+     */
     public function getErrors(): array
     {
         return $this->errors;
     }
 
+    /**
+     * @param list<JsonSchemaValidationError> $errors
+     */
     public function setErrors(array $errors): OpenApiSchemaMismatchException
     {
         $this->errors = $errors;
@@ -29,32 +45,39 @@ class OpenApiSchemaMismatchException extends \Exception
         foreach ($this->errors as $error) {
             $wildcarded = preg_replace('/\[[0-9]+\]/', '[*]', $error['property']);
 
-            $errorType = $error['constraint'] . '|' . $error['message'];
+            // ensures compatibility with both, JsonSchema 5.x and 6.x
+            $constraintName = $error['constraint']['name']
+                ?? $error['constraint'];
+
+            $errorType = sprintf('%s|%s', $constraintName, $error['message']);
             if (!array_key_exists($errorType, $errorTypeMap)) {
                 $errorTypeMap[$errorType] = [
                     'properties' => [
-                        $wildcarded => [$error['property']],
+                        $wildcarded => 1,
                     ],
-                    'constraint' => $error['constraint'],
+                    'constraint' => $constraintName,
                     'message' => $error['message'],
                 ];
             } else {
-                if (!in_array($wildcarded, $errorTypeMap[$errorType]['properties'])) {
-                    $errorTypeMap[$errorType]['properties'][$wildcarded] = [$error['property']];
+                // track usage of the same wildcard-property
+                if (!array_key_exists($wildcarded, $errorTypeMap[$errorType]['properties'])) {
+                    $errorTypeMap[$errorType]['properties'][$wildcarded] = 1;
                 } else {
-                    $errorTypeMap[$errorType]['properties'][] = $wildcarded;
+                    $errorTypeMap[$errorType]['properties'][$wildcarded]++;
                 }
             }
         }
 
         $summary = [];
-        foreach ($errorTypeMap as $es) {
-            $summary[] = sprintf('%s - %s', $es['constraint'], $es['message']);
-            foreach ($es['properties'] as $ps => $pl) {
-                if (1 == count($pl)) {
-                    $summary[] = sprintf('  - %s', $ps);
+        foreach ($errorTypeMap as $errorType) {
+            $summary[] = sprintf('%s - %s', $errorType['constraint'], $errorType['message']);
+
+            foreach ($errorType['properties'] as $propertyName => $propertyAmount) {
+                // if the property errored multiple times, add the amount to the error-message
+                if (1 === $propertyAmount) {
+                    $summary[] = sprintf('  - %s', $propertyName);
                 } else {
-                    $summary[] = sprintf('  - %s (%s more)', count($pl) - 1);
+                    $summary[] = sprintf('  - %s (%s more)', $propertyName, $propertyAmount - 1);
                 }
             }
         }
